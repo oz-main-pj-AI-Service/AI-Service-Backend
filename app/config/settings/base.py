17851d497afa48cca5186c68857f8ffa202e12fe
hak1319 <gkr054@naver.com>
2025-03-12 15:39:16 +0900

    🐛 Fix URLPATTERNS

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # OWN APP
+    # THIRD PARTY
+    "rest_framework",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
