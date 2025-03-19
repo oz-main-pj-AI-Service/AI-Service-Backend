6898122cfd5eb1936f320e3cfde8718938bf117c
hak1319 <gkr054@naver.com>
2025-03-19 21:22:17 +0900

    ⚡️ Social Login 최적화

--- app/apps/user/social_view.py
+++ app/apps/user/social_view.py
@@ -15,6 +15,51 @@ from rest_framework_simplejwt.tokens import RefreshToken
 User = get_user_model()
 
 
+def check_user_create_or_login(user, email):
+    if user and not user.is_social:  # 일반 로그인 계정이면
+        return Response(
+            {"error": "이 이메일은 포털 로그인으로 사용 중입니다."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    if user:
+        # 기존 사용자 로그인
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        store_access_token(user.id, access_token, 3600)  # redis 저장
+
+        return Response(
+            {
+                "access_token": access_token,
+                "refresh_token": str(refresh),
+                "token_type": "Bearer",
+                "expires_in": 3600,
+            },
+            status=status.HTTP_200_OK,
+        )
+    else:
+        # 새로운 사용자 생성
+        serializer = SocialUserCreateSerializer(data={"email": email})
+        if serializer.is_valid():
+            user = serializer.save()
+            user.is_active = True
+            user.save()
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            store_access_token(user.id, access_token, 3600)  # Redis에 저장
+
+            return Response(
+                {
+                    "access_token": access_token,
+                    "refresh_token": str(refresh),
+                    "token_type": "Bearer",
+                    "expires_in": 3600,
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 # class GoogleSocialLoginView(APIView):
 #     def get(self, request):
 #         client_id = settings.GOOGLE_CLIENT_ID
@@ -26,18 +71,13 @@ User = get_user_model()
 
 class GoogleSocialLoginCallbackView(APIView):
 
-    def get(self, request):
-        code = request.GET.get("code")  # 구글이 주는 인가 코드
+    def post(self, request):
+        code = request.data.get("code")  # 구글이 주는 인가 코드
         if not code:
             return Response({"error": "Authorization code is missing"}, status=400)
-        domain = (
-            os.getenv("DOMAIN")
-            if os.getenv("DOCKER_ENV", "false").lower() == "true"
-            else "127.0.0.1:8000"
-        )
+
         client_id = settings.GOOGLE_CLIENT_ID
         client_secret = settings.GOOGLE_CLIENT_SECRET
-        redirect_uri = f"http://{domain}/api/user/social-login/google/callback/"
 
         # 토큰 교환
         token_url = "https://oauth2.googleapis.com/token"
@@ -45,7 +85,6 @@ class GoogleSocialLoginCallbackView(APIView):
         data = {
             "grant_type": "authorization_code",
             "code": code,
-            "redirect_uri": redirect_uri,
             "client_id": client_id,
             "client_secret": client_secret,
         }
@@ -60,52 +99,9 @@ class GoogleSocialLoginCallbackView(APIView):
 
         email = user_info.get("email")
 
-        existing_user = User.objects.filter(email=email).first()
-
-        if existing_user and not existing_user.is_social:  # 일반 로그인 계정이면
-            return Response(
-                {"error": "이 이메일은 포털 로그인으로 사용 중입니다."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
         user = User.objects.filter(email=email).first()
-        if user:
-            # 기존 사용자 로그인
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-            store_access_token(user.id, access_token, 3600)  # redis 저장
 
-            return Response(
-                {
-                    "access_token": access_token,
-                    "refresh_token": str(refresh),
-                    "token_type": "Bearer",
-                    "expires_in": 3600,
-                },
-                status=status.HTTP_200_OK,
-            )
-        else:
-            # 새로운 사용자 생성
-            serializer = SocialUserCreateSerializer(data={"email": email})
-            if serializer.is_valid():
-                user = serializer.save()
-                user.is_active = True
-                user.save()
-                refresh = RefreshToken.for_user(user)
-                access_token = str(refresh.access_token)
-                store_access_token(user.id, access_token, 3600)  # Redis에 저장
-
-                return Response(
-                    {
-                        "access_token": access_token,
-                        "refresh_token": str(refresh),
-                        "token_type": "Bearer",
-                        "expires_in": 3600,
-                    },
-                    status=status.HTTP_200_OK,
-                )
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return check_user_create_or_login(user, email)
 
 
 # class NaverSocialLoginView(APIView):
@@ -119,19 +115,13 @@ class GoogleSocialLoginCallbackView(APIView):
 
 
 class NaverSocialLoginCallbackView(APIView):
-    def get(self, request):
-        code = request.GET.get("code")  # 네이버가 보내는 인가 코드
+    def post(self, request):
+        code = request.data.get("code")  # 네이버가 보내는 인가 코드
         if not code:
             return Response({"error": "Authorization code is missing"}, status=400)
-        state = request.GET.get("state")  # CSRF 보호를 위해 상태 토큰 확인
+        state = request.data.get("state")  # CSRF 보호를 위해 상태 토큰 확인
         client_id = settings.NAVER_CLIENT_ID
         client_secret = settings.NAVER_CLIENT_SECRET
-        domain = (
-            os.getenv("DOMAIN")
-            if os.getenv("DOCKER_ENV", "false").lower() == "true"
-            else "127.0.0.1:8000"
-        )
-        redirect_uri = f"http://{domain}/api/user/social-login/naver/callback/"
 
         # 토큰 교환
         token_url = "https://nid.naver.com/oauth2.0/token"
@@ -139,7 +129,7 @@ class NaverSocialLoginCallbackView(APIView):
         data = {
             "grant_type": "authorization_code",
             "code": code,
-            "redirect_uri": redirect_uri,
+            "state": state,
             "client_id": client_id,
             "client_secret": client_secret,
         }
@@ -155,49 +145,6 @@ class NaverSocialLoginCallbackView(APIView):
 
         email = user_info.get("response", {}).get("email")
 
-        existing_user = User.objects.filter(email=email).first()
-
-        if existing_user and not existing_user.is_social:  # 일반 로그인 계정이면
-            return Response(
-                {"error": "이 이메일은 일반 로그인으로 사용 중입니다."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
         user = User.objects.filter(email=email).first()
-        if user:
-            # 기존 사용자 로그인
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-            store_access_token(user.id, access_token, 3600)  # redis 저장
 
-            return Response(
-                {
-                    "access_token": access_token,
-                    "refresh_token": str(refresh),
-                    "token_type": "Bearer",
-                    "expires_in": 3600,
-                },
-                status=status.HTTP_200_OK,
-            )
-        else:
-            # 새로운 사용자 생성
-            serializer = SocialUserCreateSerializer(data={"email": email})
-            if serializer.is_valid():
-                user = serializer.save()
-                user.is_active = True
-                user.save()
-                refresh = RefreshToken.for_user(user)
-                access_token = str(refresh.access_token)
-                store_access_token(user.id, access_token, 3600)  # Redis에 저장
-
-                return Response(
-                    {
-                        "access_token": access_token,
-                        "refresh_token": str(refresh),
-                        "token_type": "Bearer",
-                        "expires_in": 3600,
-                    },
-                    status=status.HTTP_200_OK,
-                )
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return check_user_create_or_login(user, email)
