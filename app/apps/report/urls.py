4c53daba3b6a679ad1324e4d3f2dd442f7fd1322
hak1319 <gkr054@naver.com>
2025-03-25 00:47:33 +0900

    ⚡️ Report API 성능 향상 및 swagger 제작

--- app/apps/report/urls.py
+++ app/apps/report/urls.py
@@ -1,7 +1,13 @@
 from apps.report import views
 from django.urls import path
 
+app_name = "report"
 urlpatterns = [
     path("", views.ReportListCreateView.as_view(), name="report-list-create"),
-    path("<uuid:id>/", views.ReportDetailView.as_view(), name="report-detail"),
+    path("<uuid:report_id>/", views.ReportDetailView.as_view(), name="report-detail"),
+    path(
+        "<uuid:report_id>/admin/",
+        views.AdminReportUpdateView.as_view(),
+        name="admin-report-update",
+    ),
 ]
