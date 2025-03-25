63f08ba223319a7e13c964c94ff9b3e0a766bbf0
hak1319 <gkr054@naver.com>
2025-03-25 16:05:56 +0900

    ⚡️ Update Report Swagger

--- app/apps/report/urls.py
+++ app/apps/report/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 app_name = "report"
 urlpatterns = [
     path("", views.ReportListCreateView.as_view(), name="report-list-create"),
-    path("<uuid:report_id>/", views.ReportDetailView.as_view(), name="report-detail"),
+    path("<uuid:id>/", views.ReportDetailView.as_view(), name="report-detail"),
     path(
-        "<uuid:report_id>/admin/",
+        "<uuid:id>/admin/",
         views.AdminReportUpdateView.as_view(),
         name="admin-report-update",
     ),
