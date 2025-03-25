6e8066767a2a56972536c7368412e1f56c80e0af
hak1319 <gkr054@naver.com>
2025-03-25 23:17:48 +0900

    ✨ Report Test Code

--- app/apps/report/serializers.py
+++ app/apps/report/serializers.py
@@ -57,6 +57,16 @@ class ReportRetrieveUpdateSerializer(serializers.ModelSerializer):
 
 
 class AdminReportUpdateSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    user_id = serializers.SerializerMethodField()
+    admin_id = serializers.SerializerMethodField()
+
     class Meta:
         model = Report
         fields = "__all__"
+
+    def get_user_id(self, obj):
+        return str(obj.user_id.id) if obj.user_id else None
+
+    def get_admin_id(self, obj):
+        return str(obj.admin_id.id) if obj.admin_id else None
