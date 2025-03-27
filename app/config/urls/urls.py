f6a1c5ee806cf3c5fd4b26b103c6429be614a69a
hak1319 <gkr054@naver.com>
2025-03-27 17:09:55 +0900

    ⚡️ 개발환경 배포환경 구분

--- app/config/urls/urls.py
+++ app/config/urls/urls.py
@@ -6,11 +6,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
-if settings.DEBUG:
-    from .local_url import urlpatterns as local_urls
-
-    urlpatterns += local_urls
-
 from .base_urls import urlpatterns as base_urls
+from .local_url import urlpatterns as local_urls
 
 urlpatterns += base_urls
+
+if settings.DEBUG:
+    urlpatterns += local_urls
