19012235d88b0bfb62e5ae0eb894ff744b5fc649
hak2881 <gkr054@naver.com>
2025-04-02 13:21:54 +0900

    ⚡️로그인 시도 상태코드 전달

--- app/apps/utils/redis_block.py
+++ app/apps/utils/redis_block.py
@@ -2,7 +2,7 @@ import os
 
 import redis
 from rest_framework import status
-from rest_framework.response import Response
+from rest_framework.exceptions import APIException
 
 if os.getenv("DOCKER_ENV", "false").lower() == "true":
     REDIS_HOST = os.getenv("REDIS_HOST", "redis")
@@ -16,18 +16,18 @@ def get_login_attempt_key(key):
     return f"login_attempt_{key}"
 
 
+class TooManyLoginAttempts(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = "로그인 시도 횟수를 초과했습니다. 5분후에 다시 시도하세요."
+    default_code = "Too_much_attempts"
+
+
 def check_login_attempt_key(key, limit=5, block_time=300):
     redis_key = get_login_attempt_key(key)
     attempts = r.get(redis_key)
 
     if attempts and int(attempts) >= limit:
-        return Response(
-            {
-                "detail": "로그인 시돌 횟수를 초과했습니다. 5분후에 다시 시도하세요",
-                "code": "Too_much_attempts",
-            },
-            status=status.HTTP_403_FORBIDDEN,
-        )
+        raise TooManyLoginAttempts()
     pipe = r.pipeline()
     pipe.incr(redis_key)
     pipe.expire(redis_key, block_time)
